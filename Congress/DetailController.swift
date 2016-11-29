576f441910ab8140a89a32d3ccf51e78dbfaec79
Caleb Simmeth <simmeth@usc.edu>
2016-11-28 23:17:35 -0800

    Transfer data between pages

---
+++ Congress/DetailController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailController.swift
+//  Congress
+//
+//  Created by Caleb Simmeth on 11/28/16.
+//  Copyright © 2016 Abdominal Snowmen. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController, UITableViewDataSource {
+    
+    var details:[String] = ["a","b","c"]
+    var text:String = ""
+    @IBOutlet weak var detailsLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //TODO load details into table (just set array data?) Could do this in segue function
+        detailsLabel.text = text
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailsCell", for: indexPath)
+        cell.textLabel?.text = details[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return details.count
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Congress/FirstViewController.swift
+++ Congress/FirstViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource,UITableViewDelegate{
 
+    let data:[[String]] = [["One","Two","Three"],["Four","Five","Six"],["Seven","Eight","Nine"]]
+    var currentSelection:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,31 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stateCell", for: indexPath)
+        cell.textLabel?.text = data[0][indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("table selected")
 
+        currentSelection = indexPath.row
+        performSegue(withIdentifier: "detailsSegue", sender: Any?.self)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detail:DetailController = segue.destination as! DetailController
+        //Set data of congressmen here for detailcontroller to use
+        
+        print("setting string")
+        detail.text = String(currentSelection)
+        detail.details = data[currentSelection]
+    }
 }
 
